6cf718d2e761c3cf3ee40cc73717f13ef40c373f
Marius Ropotica <marius.ropotica@mambu.com>
2025-02-20 09:41:29 +0200

    balls in bucket, added tests, improved code

---
+++ src/main/java/org/example/other/AddTwoNumbers.java
@@ -0,0 +1,93 @@
+package org.example.other;
+
+import java.util.StringJoiner;
+
+public class AddTwoNumbers {
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        ListNode result = null;
+        ListNode l = null;
+
+        int x = 0;
+
+        while (l1 != null || l2 != null || x > 0) {
+
+            int a = l1 != null ? l1.val : 0;
+            int b = l2 != null ? l2.val : 0;
+            int val = a + b + x;
+            x = val / 10;
+            val = val % 10;
+
+            ListNode crt = new ListNode(val, null);
+            if (l == null) {
+                l = crt;
+                result = l;
+            } else {
+                l.next = crt;
+                l = crt;
+            }
+
+            l1 = l1 != null ? l1.next : null;
+            l2 = l2 != null ? l2.next : null;
+        }
+
+        return result;
+    }
+
+    private static void printList(ListNode listNode) {
+        StringJoiner stringJoiner = new StringJoiner(",");
+
+        while (listNode != null) {
+            stringJoiner.add(String.valueOf(listNode.val));
+            listNode = listNode.next;
+        }
+        System.out.println(stringJoiner);
+    }
+
+    public static ListNode toListNode(int[] array) {
+
+        ListNode l = null;
+        ListNode result = null;
+
+        for (int i : array) {
+            ListNode crt = new ListNode(i);
+            if (l == null) {
+                l = crt;
+                result = l;
+            } else {
+                l.next = crt;
+                l = crt;
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        AddTwoNumbers addTwoNumbers = new AddTwoNumbers();
+
+        ListNode l1 = toListNode(new int[]{4, 2, 3, 2, 1});
+        ListNode l2 = toListNode(new int[]{6, 2, 3, 2, 1, 1, 2, 3});
+        ListNode result = addTwoNumbers.addTwoNumbers(l1, l2);
+
+        printList(result);
+    }
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
---
+++ src/test/java/org/example/other/AddTwoNumbersTest.java
@@ -0,0 +1,44 @@
+package org.example.other;
+
+import static org.example.other.AddTwoNumbers.toListNode;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.stream.Stream;
+
+import org.example.other.AddTwoNumbers.ListNode;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class AddTwoNumbersTest {
+
+    @ParameterizedTest
+    @MethodSource("testCases")
+    public void addTwoNumbers(ListNode n1, ListNode n2, ListNode expected) {
+
+        AddTwoNumbers addTwoNumbers = new AddTwoNumbers();
+
+        ListNode actual = addTwoNumbers.addTwoNumbers(n1, n2);
+
+        assertTrue(equal(actual, expected));
+    }
+
+    public static Stream<Arguments> testCases() {
+        return Stream.of(
+                Arguments.of(toListNode(new int[]{1, 3, 4, 2}), toListNode(new int[]{1, 3, 4, 2}), toListNode(new int[]{2, 6, 8, 4})),
+                Arguments.of(toListNode(new int[]{5, 4, 5, 7}), toListNode(new int[]{9, 8, 8, 2, 9, 1}), toListNode(new int[]{4, 3, 4, 0, 0, 2}))
+        );
+    }
+
+    private boolean equal(ListNode l1, ListNode l2) {
+        while (l1 != null && l2 != null) {
+            if (l1.val != l2.val) {
+                return false;
+            }
+            l1 = l1.next;
+            l2 = l2.next;
+        }
+
+        return l1 == null && l2 == null;
+    }
+}
